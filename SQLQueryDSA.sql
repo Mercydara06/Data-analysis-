8bd5341c10a87bdaae8e02da0cf96343b3125b90
Mercydara06 <mercydara1403@gmail.com>
2025-07-06 14:50:23 +0100

    Add files via upload

---
+++ SQLQueryDSA.sql
@@ -0,0 +1,25 @@
+create table KMS(
+Row_ID VARCHAR(50),
+Order_ID VARCHAR(50),
+Order_Date DATE,
+Order_Priority VARCHAR(50),
+Order_Quantity INT,
+Sales DECIMAL(10,2),
+Discount DECIMAL(5,2),
+Ship_Mode VARCHAR(50),
+Profit DECIMAL(10,2),
+Unit_Price DECIMAL(10,2),
+Shipping_Cost DECIMAL(10,2),
+Customer_Name VARCHAR(100),
+Province VARCHAR(50),
+Region VARCHAR(50),
+Customer_Segment VARCHAR(50),
+Product_Category VARCHAR(50),
+Product_SubCategory VARCHAR(50),
+Product_Name TEXT,
+Product_Container VARCHAR(50),
+Product_Base DECIMAL(10,2),
+Ship_Date DATE
+);
+
+SELECT 
